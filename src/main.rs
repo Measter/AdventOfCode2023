ff52a43a74bbd607c7922ce3b2e2e52545bff6e7
Stuart Haidon <serayen.sh@gmail.com>
2023-11-28 17:13:10 +0000

    Add Chumsky and Logos for parsing, supertrait for Chumsky.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,15 @@ mod days;
 #[global_allocator]
 static ALLOC: TracingAlloc = TracingAlloc;
 
+#[cfg(feature = "testing")]
+type ErrType<'a> = chumsky::extra::Default;
+
+#[cfg(not(feature = "testing"))]
+type ErrType<'a> = chumsky::extra::Err<chumsky::error::Rich<'a, char>>;
+
+trait Parser<'a, T>: chumsky::Parser<'a, &'a str, T, ErrType<'a>> {}
+impl<'a, O, T> Parser<'a, O> for T where T: chumsky::Parser<'a, &'a str, O, ErrType<'a>> {}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     aoc_lib::run(&ALLOC, 2023, days::DAYS)?;
