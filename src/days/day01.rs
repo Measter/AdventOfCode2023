7023a265f741d8bf7fe9466227b9fdba65bba9e7
Stuart Haidon <serayen.sh@gmail.com>
2023-12-01 21:11:37 +0000

    Add perfect-hash-based day 1 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -6,7 +6,10 @@ pub const DAY: Day = Day {
     name: "Trebuchet!?",
     part_1: run_part1,
     part_2: Some(run_part2),
-    other: &[("Parse", run_parse)],
+    other: &[
+        ("Parse", run_parse),
+        ("Perfect Hash (Part 2)", run_perf_hash),
+    ],
 };
 
 fn run_part1(input: &str, b: Bench) -> BenchResult {
@@ -24,6 +27,10 @@ fn run_parse(input: &str, b: Bench) -> BenchResult {
         let data = parse(input).map_err(UserError)?;
         Ok::<_, Report>(ParseResult(data))
     })
+}
+
+fn run_perf_hash(input: &str, b: Bench) -> BenchResult {
+    b.bench(|| Ok::<_, NoError>(perf_hash(input)))
 }
 
 fn parse(input: &str) -> Result<Vec<&str>> {
@@ -107,6 +114,92 @@ fn part2(lines: &[&str]) -> u16 {
             left.unwrap_or_default() * 10 + right.unwrap_or_default()
         })
         .sum()
+}
+
+fn perf_hash(input: &str) -> u16 {
+    const LOOKUP: [u8; 256] = [
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 4, 5, 0, 31, 31, 31, 31, 31, 31, 15, 15, 31, 31, 10, 0,
+        10, 10, 5, 31, 0, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31,
+        31, 31,
+    ];
+
+    let mut sum = 0;
+    let mut left = None;
+    let mut right = 0;
+    let mut bytes = input.as_bytes();
+
+    'outer: while !bytes.is_empty() {
+        match bytes {
+            [b'\n', ..] => {
+                sum += left.unwrap_or_default() * 10 + right;
+                left = None;
+            }
+            [b @ b'0'..=b'9', ..] => {
+                left = left.or(Some((*b - b'0') as u16));
+                right = (*b - b'0') as u16;
+            }
+            [a, _, b, ..] => {
+                const WORDS: [(u16, &str); 31] = [
+                    (6, "six"),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (8, "eight"),
+                    (7, "seven"),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (5, "five"),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (4, "four"),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (1, "one"),
+                    (3, "three"),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (2, "two"),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (0, ""),
+                    (9, "nine"),
+                ];
+                let key = LOOKUP[*a as usize] + LOOKUP[*b as usize];
+                'block: {
+                    let Some((val, word)) = WORDS.get(key as usize) else {
+                        break 'block;
+                    };
+                    if !word.is_empty() && bytes.starts_with(word.as_bytes()) {
+                        left = left.or(Some(*val));
+                        right = *val;
+                        bytes = &bytes[word.len() - 1..];
+                        continue 'outer;
+                    }
+                }
+            }
+            _ => {}
+        }
+        bytes = &bytes[1..];
+    }
+
+    sum
 }
 
 #[cfg(test)]
